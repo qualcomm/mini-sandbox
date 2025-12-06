0b26b805234c2a82536ef025394d9908e35d456b
amantova <amantova@qti.qualcomm.com>
2025-12-05 22:15:48 -0800

    test
    Signed-off-by: amantova <amantova@qti.qualcomm.com>

--- setenv.sh
+++ setenv.sh
@@ -4,6 +4,7 @@
 ## SPDX-License-Identifier: MIT
 ##
 
+
 script_path=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 sandbox_path="$script_path/mini_sandbox/src/main/tools/out"
 echo "Adding $sandbox_path into PATH"
