77c3fbbfd8f302ee93d5e6a306dce61449e0de96
amantova <amantova@qti.qualcomm.com>
2025-12-18 07:05:04 -0800

    run_all.sh remove test file

--- mini_sandbox/utils/test/client_libmini_tapbox/run_all.sh
+++ mini_sandbox/utils/test/client_libmini_tapbox/run_all.sh
@@ -58,7 +58,7 @@ rm -f $HOME/test2
 check_exit $SCRIPT_DIR/client_max_connection.bin
 ls "$HOME/test2"
 check_last_command
-
+rm -f $HOME/test2
 
 # THis is going to use the dynamic linker so if the lib is not installed system-wide
 # You'll have to export LD_LIBRARY_PATH=/path/to/libs/
