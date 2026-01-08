81fc00e0ab837489e59a9cbe9531a8197dc59bc0
amantova <amantova@qti.qualcomm.com>
2026-01-08 11:17:16 -0800

    remove mini_sandbox_allow_max_connections from pyminitapbox cause it is already defined in the internal module

--- mini_sandbox/src/py/ctype_bindings/pyminitapbox.py
+++ mini_sandbox/src/py/ctype_bindings/pyminitapbox.py
@@ -6,10 +6,3 @@
 from internal_mini_sandbox import *
 
 init(tap_mode = True)
-
-def mini_sandbox_allow_max_connections(max_connections):
-    if lib is None:
-        return -1
-    return lib.mini_sandbox_allow_max_connections(max_connections)
-
-
