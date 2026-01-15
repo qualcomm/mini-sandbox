e26b4e05c9f299aa859db5199fe333544717dd0c
amantova <amantova@qti.qualcomm.com>
2026-01-15 10:30:50 -0800

    reduce attack surface by not mounting all /dev but instead only mounting basic stuff like /dev/null, /dev/stdout etc. See MountDev() for details. This commit also fixes a problem we had in read-only mode only when mounting the cwd over itself and the cwd was corresponding to a mount point itself. Finally add a test case for read-only mode and helper to set the env locally for testing
    Signed-off-by: amantova <amantova@qti.qualcomm.com>

---
+++ mini_sandbox/utils/test/cli_tests/test_readonly.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+##
+## Copyright (c) 2025 Qualcomm Technologies, Inc. and/or its subsidiaries.
+## SPDX-License-Identifier: MIT
+##
+
+mini-sandbox -- /bin/bash << 'EOF'
+
+check_last_command() {
+    if [ $? -ne 0 ]; then
+        echo "Error: Last command failed."
+        exit 1
+    else
+        echo "Success: Last command succeeded."
+    fi
+}
+
+
+check_last_command_failed() {
+    if [ $? -ne 0 ]; then
+        echo "Success: Last command failed (As Expected)."
+        return 0
+    else
+        echo "Error: Last command succeeded but should have failed."
+        exit 1
+    fi
+}
+
+
+check_ls_count() {
+    local count="$1"
+    local threshold="$2"
+
+    if [ "$count" -gt "$threshold" ]; then
+        echo "Success: Directory contains more than $threshold items ($count)."
+        return 0
+    else
+        echo "Error: Directory contains $count items, which is not more than $threshold."
+        exit 1
+    fi
+}
+
+
+echo -e "\nTest writing in HOME dir"
+echo "sandbox-test" > $HOME/test.txt
+check_last_command_failed
+
+echo -e "\nTest writing in working dir"
+echo "sandbox-test" > ./test.txt
+check_last_command
+rm -rf ./test.txt
+EOF
+
+exit $?
