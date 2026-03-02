3a1d6bbe684872607c3065d1f68a14c741106583
Raffaele Stelluti <rstellut@qti.qualcomm.com>
2026-03-02 08:49:58 -0800

    Fix errorcode in test_err.py

--- mini_sandbox/utils/test/python_test/test_err.py
+++ mini_sandbox/utils/test/python_test/test_err.py
@@ -32,9 +32,6 @@ if __name__ == "__main__":
     assert(res==0)
     res=mn_sbx.mini_sandbox_start()
     err_code=mn_sbx.mini_sandbox_get_last_error_code()
-    assert(err_code == mn_sbx.MiniSandboxErrors.NESTED_SANDBOX)
-
-    print("Ok")
-
+    assert(err_code == mn_sbx.MiniSandboxErrors.SANDBOX_ALREADY_STARTED)
 
 
